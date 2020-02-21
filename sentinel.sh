2ecf8ae4436f030a0ff0fec23c1c897a5e26881f
Mark Fynes <tfynes@pivotal.io>
2020-02-21 14:33:16 -0500

    added content-length:0 and keep-alive:close headers

--- sentinel.sh
+++ sentinel.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-GOOD_RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
-BAD_RESPONSE="HTTP/1.1 404 Not Found: url does not exist\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
+GOOD_RESPONSE="HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: 0\r\n\r\n${2:-"OK"}\r\n"
+BAD_RESPONSE="HTTP/1.1 404 Not Found: url does not exist\r\nContent-Length: 0\r\nConnection: close\r\n\r\n${2:-"OK"}\r\n"
 
 while 	{ 
 	check=GOOD
